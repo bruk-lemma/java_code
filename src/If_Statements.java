3a86b8b25dbe20f3f90afd9ed906a0ed77686962
bruk-lemma <bruklemma2017@gmail.com>
2022-05-10 22:15:31 +0300

    implemented the switch case another way to implement if else cases;

--- src/If_Statements.java
+++ src/If_Statements.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class If_Statements {
     public static  void main(String[] args){
         //the following code demonstrates the basic use of if-else clause
@@ -14,5 +16,22 @@ public class If_Statements {
         int income=120_000;
         String classType= (income > 100_000? "Business-class" : "Economy-class");
         System.out.println("class-type = " + classType);
+
+        //the following code demonstrates how to use the switch statements another alternative to if-else statements in java
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("what's is your role in the company!! ");
+        String role=scanner.nextLine();
+        switch(role){
+            case "admin":
+                System.out.println("your role is As An Admin...!");
+                break;
+            case "user":
+                System.out.println("your role is as a User...!");
+                break;
+            default:
+                System.out.println("Your are a guest....!");
+        }
+
     }
+
 }
