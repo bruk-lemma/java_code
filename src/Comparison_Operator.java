9f5d9f722e321b2412c896972daf9306fa5fa5b4
bruk-lemma <bruklemma2017@gmail.com>
2022-05-10 19:13:55 +0300

    created s simple comparison operator in java.

---
+++ src/Comparison_Operator.java
@@ -0,0 +1,8 @@
+public class Comparison_Operator {
+    public static  void main(String[] args){
+     int x=2;
+     int y=2;
+        System.out.println(x>y);
+        System.out.println(x<=y);
+        System.out.println(x==y);    }
+}
